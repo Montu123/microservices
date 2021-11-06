e634c93b3f0c88f51e517c077a3a5cbf11386aa0
Sandeep Singh <sandeep.singh@73String.com>
2021-11-06 17:06:12 +0530

    initial-project

---
+++ JPA-MS-Test/src/main/java/com/spring/jpatest/JpaMsTestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMsTestApplication.class, args);
+	}
+
+}
